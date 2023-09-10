bc45d5bfa013054ce2fab38cc63bd7cb56de8e8b
alex <alex@alexs-Mac-mini.local>
2023-09-10 19:25:02 +0200

    fix request comp import

--- src/components/Requests.vue
+++ src/components/Requests.vue
@@ -6,19 +6,21 @@
 
 import useUser from '@/composables/user.composable'
 import { defineAsyncComponent,computed } from 'vue';
+import UserRequestIndex from './request/user/RequestIndex.vue'
+import AdminRequestIndex from './request/admin/RequestIndex.vue'
 const {user}=useUser()
 
 const selectedComponent =computed(()=>{
     const requestView={
-        "user":'./request/user/RequestIndex.vue',
-        "admin":'./request/admin/RequestIndex.vue'
+        "user":UserRequestIndex,
+        "admin":AdminRequestIndex
     }
     
     //mostra la view corretta in base al ruolo
     let role=user.value.role.toLowerCase()
     let comp=requestView[role] || requestView.user
     
-    return defineAsyncComponent(()=>import(comp))
+    return comp
 }) 
 
 

3fb0233ed6aaa054fd9afd3e67fdc7eb17f61a27
alex <alex@alexs-Mac-mini.local>
2023-10-21 12:43:55 +0200

    fix school index status

--- src/components/school/SchoolIndex.vue
+++ src/components/school/SchoolIndex.vue
@@ -18,6 +18,7 @@
                         <th>Indirizzo</th>
                         <th>Sezione</th>
                         <th>Discipline</th>
+                        <th>Stato</th>
                         <th>      
                             <button type="button"  class="btn btn-outline-success" v-if="isAdmin" style="width: 50%;" @click.prevent="showDumpStudents(0)">Dump</button>
                         </th>
@@ -29,16 +30,17 @@
                         <td>{{ s.data.sc_tab_indirizzo }}</td>
                         <td>{{ utils.parseZone(s.data.sc_tab_plesso_code) }}</td>
                         <td><span v-html="utils.formatDiscipline(Object.keys(s.discipline))"></span></td>
+                        <td>{{ s.status=='NOT_CONFIRMED' ? 'NON CONFERMATO' : 'CONFERMATO' }}</td>
                         <td>
-                           
-                            <div class="action" v-if="!isDocente">
                             
+                            <div class="action" v-if="isAdmin">
+                                
                                 <button type="button" :disabled="!s.students.length" class="btn btn-outline-success" v-if="isAdmin" @click.prevent="showDumpStudents(s.id)">Dump</button>
                                 <button type="button" class="btn btn-primary btn-ms" @click.prevent="showEditSchool(s.id)">Modifica</button>
                                 <router-link class="btn btn-outline-primary" :to="{ name: 'student.index', params: { 'schoolId': s.id }}">Studenti</router-link>
                             
                             </div>
-                            <div class="action" v-else>
+                            <div class="action" v-else-if="isDocente">
                                 
                                 <section v-if="s.status!='CONFIRMED'">
                                     <a href="#link" class="btn btn-outline-primary" @click.prevent="doConfirmSchool(s)" role="button">Conferma Partecipazione</a>
